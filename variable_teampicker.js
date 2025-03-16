498ad82592b2fbab7a1c744133a6c967aead9688
Josh <reimer.josh.2006@gmail.com>
2025-03-16 16:21:09 -0300

    bringin repo up to date with public site

--- variable_teampicker.js
+++ variable_teampicker.js
@@ -1,3 +1,6 @@
+window.addEventListener('load', () => {
+  clearfields("app.js");
+});
 var textarea = document.getElementById("names");
 var team_divs = document.getElementsByClassName("team_divs");
 //is a list^
@@ -18,13 +21,29 @@ function auto_grow(element) {
 textarea.style.height = 0;
 textarea.style.height = textarea.scrollHeight + "px";
 
-function clearfields() {
-  while (team_divs.length != 0) {
+function clearfields(origin) {
+  if(origin === "textarea" && originalNames !== null){
+    //show warning
+    let warning_popup = document.getElementById("warning-popup");
+    let clear_button = document.getElementById("clear-button");
+    let cancel_button = document.getElementById("cancel-button");
+    warning_popup.classList.add('show-warning');
+    textarea.blur();
+    clear_button.onclick = function() {
+        while (team_divs.length != 0) {
     team_divs[0].remove();
   }
   textarea.value = '';
+  originalNames = null; // Reset originalNames when clearing fields
   textarea.style.height = 0;
   textarea.style.height = textarea.scrollHeight + "px";
+  warning_popup.classList.remove("show-warning");
+      }
+    cancel_button.onclick = function(){
+      warning_popup.classList.remove("show-warning");
+    }
+  }
+  
 }
 
 function shrink(element) {
@@ -59,84 +78,55 @@ function pickTeams(team_members, number_of_teams) {
     //or, nth shuffled member
     teams.push(shuffled_members.slice(i, i + team_length));
   }
+
   return teams;
 }
 
 var textareaValue;
+var originalNames = null; // Store the original list of names
 function getTeams() {
   check_radio_buttons();
-  if(textarea.value.includes("<") || textarea.value.includes(">")){
-    alert("You cannot use < or > special characters");
-  } else {
-  if (textarea.value.replace(/\s+/g, '') != '') {
-    //if there is a non white space value in the input box
-
-    if (team_divs[0] == null) {
-      //if the random teams that were generated are
-      //not present, generate new teams
-
-      textareaValue = textarea.value.split("\n");
-      check_radio_buttons();
-      if (number_of_teams > textareaValue.length) {
-        //make a popup and dont calculate teams
-        let alert_text = `You cannot split ${textareaValue.length} team members into ${number_of_teams} teams`;
-        alert(alert_text);
-
-        return null;
-      }
-
-
-      check_radio_buttons();
-      let teams = pickTeams(textareaValue, number_of_teams);
-
-      textarea.insertAdjacentHTML("afterend", getTeamsHTML(teams.slice(1,)));
-
-      let textarea_output_string = "";
-      for (let e = 0; e < teams[0].length; e++) {
-        if (e != (teams[0].length-1)) {
-          textarea_output_string += teams[0][e]+"\n";
-        } else {
-          textarea_output_string += teams[0][e];
-        }
-      }
-
-      textarea.value = textarea_output_string;
-      textarea.setAttribute("style", "height:" + (textarea.scrollHeight) + "px;overflow-y:hidden;");
-      shrink(textarea);
-    } else {
-      //if there are teams already generated,
-      //regenerate them
-      check_radio_buttons();
-      if (number_of_teams > textareaValue.length) {
-        //make a popup and dont calculate teams
-        let alert_text = `You cannot split ${textareaValue.length} team members into ${number_of_teams} teams`;
-        alert(alert_text);
-
-        return null;
-      }
-      clearfields();
-
-      teams_second_time = pickTeams(textareaValue, number_of_teams);
-
-      textarea.insertAdjacentHTML("afterend", getTeamsHTML(teams_second_time.slice(1,)));
-      let textarea_output_string = "";
-
-      for (let e = 0; e < teams_second_time[0].length; e++) {
-        if (e != (teams_second_time[0].length-1)) {
-          textarea_output_string += teams_second_time[0][e]+"\n";
-        } else {
-          textarea_output_string += teams_second_time[0][e];
-        }
-      }
-
-      textarea.value = textarea_output_string;
-      textarea.setAttribute("style", "height:" + (textarea.scrollHeight) + "px;overflow-y:hidden;");
-      shrink(textarea);
-    }
+  
+  // If originalNames is null, this is our first run, so store the original names
+  if (originalNames === null) {
+    originalNames = textarea.value
+      .split('\n')
+      .filter(line => line.trim() !== '');  // Remove empty lines and whitespace-only lines
   }
-}
-}
 
+  if (originalNames.some(name => name.includes("<") || name.includes(">"))) {
+    alert("You cannot use < or > special characters");
+    return;
+  }
+
+  if (originalNames.length === 0) {
+    return; // Exit if no names
+  }
+
+  check_radio_buttons();
+  if (number_of_teams > originalNames.length) {
+    let alert_text = `You cannot split ${originalNames.length} team members into ${number_of_teams} teams`;
+    alert(alert_text);
+    return;
+  }
+
+  // Clear any existing team divs
+  while (team_divs.length > 0) {
+    team_divs[0].remove();
+  }
+
+  // Generate new teams using the original names
+  let teams = pickTeams(originalNames, number_of_teams);
+  
+  // Display the teams
+  textarea.insertAdjacentHTML("afterend", getTeamsHTML(teams.slice(1)));
+
+  // Update textarea with first team
+  let textarea_output_string = teams[0].join("\n");
+  textarea.value = textarea_output_string;
+  textarea.setAttribute("style", "height:" + (textarea.scrollHeight) + "px;overflow-y:hidden;");
+  shrink(textarea);
+}
 function getTeamsHTML(teamlist) {
   /*
   this function loops through every list of teams in the list of teams called teamlist.
