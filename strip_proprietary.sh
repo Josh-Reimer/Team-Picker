01b207414706a8c8e9ff2b6e76fee4898fb6e86b
Josh <reimer.josh.2006@gmail.com>
2025-03-18 20:38:24 -0300

    improved private code script

--- strip_proprietary.sh
+++ strip_proprietary.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
 # Define the files to keep in the public repo
-PUBLIC_FILES=("index.html" "app.js" "index.css")
+PUBLIC_FILES=("index.html" "app.js" "index.css" ".gitignore")
 
-# Remove proprietary code and its comment markers from the selected public files
+# Remove proprietary code and public code comment markers from the selected public files
 for FILE in "${PUBLIC_FILES[@]}"; do
     if [[ -f "$FILE" ]]; then
         # Remove everything between PROPRIETARY CODE START and PROPRIETARY CODE END, including markers
         sed -i '/PROPRIETARY CODE START/,/PROPRIETARY CODE END/d' "$FILE"
+        
+        # Remove public code markers as well
+        sed -i '/PUBLIC CODE START/d' "$FILE"
+        sed -i '/PUBLIC CODE END/d' "$FILE"
     fi
 done
 
 # Delete all other files EXCEPT the ones listed above and preserve .git directory
-find . -type f ! -name "index.html" ! -name "app.js" ! -name ".git*" ! -name "strip_proprietary.sh" -delete
+find . -type f \
+    ! -name "index.html" \
+    ! -name "app.js" \
+    ! -name "index.css" \
+    ! -name ".gitignore" \
+    ! -name "strip_proprietary.sh" \
+    ! -path "./.git/*" \
+    -delete
 
-echo "Proprietary code and markers removed. Only index.html + app.js + index.css retained for public release."
+# Delete the images folder and its contents
+rm -rf images
+
+# Delete the .vscode folder and its contents
+rm -rf .vscode
+
+echo "Proprietary and public code markers removed. Only index.html, app.js, index.css, and .gitignore retained. .git preserved. Images and .vscode folders deleted."
